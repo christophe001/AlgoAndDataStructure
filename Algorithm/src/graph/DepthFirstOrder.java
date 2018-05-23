d99438c7c98c7558029e63cae0a375068883e656
chris <chris@LAPTOP-QK408257>
2018-05-23 15:31:56 -0400

    Make changes to graph

--- Algorithm/src/graph/DepthFirstOrder.java
+++ Algorithm/src/graph/DepthFirstOrder.java
@@ -2,31 +2,124 @@ package graph;
 
 import datastructure.*;
 
-public class DepthFirstOrder {
+import stdlib.*;
 
-	
-	private Stack<Integer> postOrder;
-	private boolean[] visited;
-	
-	public DepthFirstOrder(Digraph digraph) {
-		this.visited = new boolean[digraph.V()];
-		this.postOrder = new Stack<Integer>();
-		for (int v = 0; v < digraph.V(); v++) {
-			if (!visited[v]) dfs(digraph, v);
-		}
+public class DepthFirstOrder {
+	 private boolean[] marked;          // marked[v] = has v been marked in dfs?
+	 private int[] pre;                 // pre[v]    = preorder  number of v
+	 private int[] post;                // post[v]   = postorder number of v
+	 private Queue<Integer> preorder;   // vertices in preorder   
+	 private Queue<Integer> postorder;  // vertices in postorder
+	 private int preCounter;            // counter or preorder numbering   
+	 private int postCounter;           // counter for postorder numbering
+
+
+	    
+	 public DepthFirstOrder(Digraph G) {
+		 pre    = new int[G.V()];
+		 post   = new int[G.V()];	     
+		 postorder = new Queue<Integer>();	     
+		 preorder  = new Queue<Integer>();
+		 marked    = new boolean[G.V()];
+		 for (int v = 0; v < G.V(); v++)
+			 if (!marked[v]) dfs(G, v);
+		 assert check();       
+	 }   
+	 
+	 public DepthFirstOrder(EdgeWeightedDigraph G) {        
+		 pre    = new int[G.V()];	        
+		 post   = new int[G.V()];	        
+		 postorder = new Queue<Integer>();
+		 preorder  = new Queue<Integer>();
+		 marked    = new boolean[G.V()];
+		 for (int v = 0; v < G.V(); v++)
+			 if (!marked[v]) dfs(G, v);
 	}
-	
-	public void dfs(Digraph digraph, int v) {
-		visited[v] = true;
-		for (int w : digraph.adj(v)) {
-			if (!visited[w]) dfs(digraph, w);
-		}
-		postOrder.push(v);
-	}
-	
-	public Iterable<Integer> reversePost() { 
-		return postOrder; 
-	}
+
+	    // run DFS in digraph G from vertex v and compute preorder/postorder
+	    
+	 private void dfs(Digraph G, int v) {
+		 marked[v] = true;	        
+		 pre[v] = preCounter++;	        
+		 preorder.enqueue(v);	        
+		 for (int w : G.adj(v)) {	            
+			 if (!marked[w]) {	                
+				 dfs(G, w);
+	         }	        
+		 }	        
+		 postorder.enqueue(v);	        
+		 post[v] = postCounter++;	   
+	 }
+
+	    // run DFS in edge-weighted digraph G from vertex v and compute preorder/postorder	    
+	 private void dfs(EdgeWeightedDigraph G, int v) {	        
+		 marked[v] = true;	        
+		 pre[v] = preCounter++;	        
+		 preorder.enqueue(v);	        	        	        
+		 for (DirectedEdge e : G.adj(v)) {	            
+			 int w = e.to();	           
+			 if (!marked[w]) {	                
+				 dfs(G, w);	            
+			 }	        
+		 }	        
+		 postorder.enqueue(v);	        
+		 post[v] = postCounter++;	    
+	 }
+	 
+	 public int pre(int v) {	        
+		 validateVertex(v);	        
+		 return pre[v];	    
+	 }
+	 	    
+	 public int post(int v) {	        
+		 validateVertex(v);	        
+		 return post[v];	    
+	 }
+	    
+	 public Iterable<Integer> post() {        
+		 return postorder;    
+	 }
+	    
+	 public Iterable<Integer> pre() {        
+		 return preorder;    
+	 }
+    
+	 public Iterable<Integer> reversePost() {	        
+		 Stack<Integer> reverse = new Stack<Integer>();        
+		 for (int v : postorder)            
+			 reverse.push(v);	        
+		 return reverse;    
+	 }
+	    // check that pre() and post() are consistent with pre(v) and post(v)
+	    
+	 private boolean check() {
+        // check that post(v) is consistent with post()        
+		 int r = 0;     
+		 for (int v : post()) {	            
+			 if (post(v) != r) {	                
+				 StdOut.println("post(v) and post() inconsistent");	                
+				 return false;	           
+			 }           
+			 r++;        
+		 }
+	        // check that pre(v) is consistent with pre(        
+		 r = 0;	        
+		 for (int v : pre()) {            
+			 if (pre(v) != r) {	               
+				 StdOut.println("pre(v) and pre() inconsistent");	               
+				 return false;	            
+			 }            
+			 r++;	        
+		 }
+		 return true;	    
+	 }
+
+	    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+	 private void validateVertex(int v) {
+		 int V = marked.length;
+		 if (v < 0 || v >= V)
+			 throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+	 }
 
 	public static void main(String[] args) {
 		Digraph dg = new Digraph(7);
